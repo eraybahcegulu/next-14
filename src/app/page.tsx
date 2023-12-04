01abd62914eacbe62d1e860798e0cfea57af049e
Eray BAHÇEGÜLÜ <eraybahcegulu@gmail.com>
2023-12-04 15:40:19 +0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { ConfigProvider} from 'antd';
+import { ConfigProvider } from 'antd';
 import theme from './theme/themeConfig'
 
 import Index from "./components/Index"
@@ -9,7 +9,16 @@ export default function Home() {
 
   return (
     <ConfigProvider theme={theme}>
-      <Index/>
+      <div className='h-full w-full border border-red-700 p-4 lg:flex flex-col gap-4'>
+        <div className='h-[10vh] min-h-[50px]w-full  border border-black flex items-center justify-center'>
+          <span className='text-4xl font-bold text-center'>APP</span>
+        </div>
+        <div className='h-full w-full mt-4 lg:mt-0'>
+          <Index />
+        </div>
+
+      </div>
+
     </ConfigProvider>
   )
 }
