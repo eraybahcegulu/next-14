eb15ee1a9ecea32f5690c3ec87a25ecb1e4ba74d
Eray BAHÇEGÜLÜ <eraybahcegulu@gmail.com>
2023-12-05 09:17:50 +0300

    edit by selected id, update selected id

--- src/app/components/Index.tsx
+++ src/app/components/Index.tsx
@@ -1,13 +1,18 @@
 'use client';
-import { Button, Card, Form, Input } from 'antd';
+import { Button, Card, Form, FormInstance, Input } from 'antd';
 import axios from "axios";
 import { useEffect, useState } from 'react';
-import { CloseOutlined } from '@ant-design/icons';
+import { ArrowRightOutlined, CheckOutlined, CloseOutlined, EditOutlined } from '@ant-design/icons';
+import Link from 'next/link';
 
 const Index: React.FC = () => {
 
   const [titles, setTitles] = useState<any[]>([]);
   const [titleForm] = Form.useForm();
+
+  const [selectedTitleId, setSelectedTitleId] = useState<string | null>(null);
+  const [isEditTitle, setIsEditTitle] = useState<boolean | null>(null);
+  const [updatedTitle, setUpdatedTitle] = useState<string | null>(null);
 
   const fetchTitles = async () => {
     try {
@@ -24,11 +29,33 @@ const Index: React.FC = () => {
       `http://localhost:3000/api/titles/delete/${id}`
     );
     fetchTitles();
+    setIsEditTitle(false);
+  };
+
+  const updateTitle = async (id: any) => {
+    await axios.put(
+      `http://localhost:3000/api/titles/update/${id}`,
+      { name: updatedTitle } 
+    );
+    fetchTitles();
+    setUpdatedTitle(null);
+    setIsEditTitle(false);
   };
 
   useEffect(() => {
     fetchTitles();
+    setIsEditTitle(false);
   }, []);
+
+  const editTitle = async (id: any) => {
+    setSelectedTitleId(id);
+    setIsEditTitle(true);
+    setUpdatedTitle(null);
+    const titleToEdit = titles.find((title) => title._id === id);
+    if (titleToEdit) {
+      setUpdatedTitle(titleToEdit.name);
+    }
+  };
 
   const onFinish = async (values: any) => {
 
@@ -45,11 +72,10 @@ const Index: React.FC = () => {
   };
 
   return (
-    <div className='border border-black h-full w-full p-6 flex flex-col lg:flex-row justify-between gap-10'>
-      <div className='border border-black h-full min-h-[78vh] w-full flex flex-col gap-2 items-center justify-center p-4' >
-
+    <div className='border h-full min-h-[80vh] w-full p-6 flex flex-col items-center justify-center gap-10'>
+      <div className='p-2 bg-slate-200'>
         <Form
-          className="mt-4 flex flex-col"
+          className="flex flex-col"
           layout="vertical"
           onFinish={onFinish}
           form={titleForm}
@@ -72,38 +98,72 @@ const Index: React.FC = () => {
             </Button>
           </Form.Item>
         </Form>
+      </div>
 
-        <Card className='overflow-y-auto' style={{ width: 300, height: 300 }}>
-          <ul className='flex flex-col gap-2 text-center'>
 
-            {titles.map((title) => (
-              <div key={title._id} className='flex flex-row justify-between gap-2'>
-                <li className='p-2 bg-teal-600 text-white w-full' >
-                  <span>{title.name.toUpperCase()}</span>
-                </li>
-                <div className='flex flex-col items-center justify-center'>
+      <Card className='overflow-y-auto' style={{ width: 300, height: 300, backgroundColor: 'wheat' }}>
+        <ul className='flex flex-col gap-2 text-center'>
+
+          {titles.map((title) => (
+            <div key={title._id} className='flex flex-row justify-between gap-2'>
+
+              <li className='p-2 bg-teal-600 text-white w-full' >
+                <span>
+                  {
+                    isEditTitle && selectedTitleId === title._id
+                      ?
+                      <Input style={{ borderRadius: "0" }} size="small" 
+                      onChange={(e) => setUpdatedTitle(e.target.value)}
+                      defaultValue={ title.name } />
+                      :
+                      title.name
+                  }
+                </span>
+              </li>
+
+              <div className='flex flex-col items-center justify-center'>
+                <span className='cursor-pointer'
+                  onClick={() => deleteTitle(title._id)}
+                > <CloseOutlined /> </span>
+              </div>
+
+              <div className='flex flex-col items-center justify-center'>
+
+                {
+                  isEditTitle
+                  ?
+                  (
+                    selectedTitleId === title._id
+                    ?
+                    <span className='cursor-pointer text-blue-500'
+                    onClick={() => updateTitle(title._id)}
+                    > <CheckOutlined /></span>
+                    :
+                    <span className='text-gray-400'
+                    > <EditOutlined /> </span>
+                  )
+                  :
                   <span className='cursor-pointer'
-                    onClick={() => deleteTitle(title._id)}
-                  > <CloseOutlined /> </span>
-                </div>
+                  onClick={() => editTitle(title._id)}
+                  > <EditOutlined /> </span>
+                }
+                
+              </div>
+
+              <div className='flex flex-col items-center justify-center'>
+
+                <Link href={`/titles/${title._id}`}>
+                  <span className='cursor-pointer'
+                  > <ArrowRightOutlined /> </span>
+                </Link>
 
               </div>
 
-            ))}
-          </ul>
-        </Card>
-      </div>
+            </div>
 
-
-      <div className='border border-black h-full min-h-[78vh] w-full flex flex-col gap-2 items-center justify-center p-4'>
-        <Card className='overflow-y-auto' style={{ width: 300, height: 300 }}>
-          <ul className='flex flex-col gap-2 text-center'>
-            <li className='p-2 bg-teal-600 text-white'> a </li>
-
-          </ul>
-        </Card>
-
-      </div>
+          ))}
+        </ul>
+      </Card>
     </div>
   )
 }
